6e1287b376ebf3a22ee33e282a6a859d9d4b520a
Chao Li <chnlich@live.com>
2026-03-07 16:46:16 -0500

    Revert "fix(server): bind to localhost instead of all interfaces"
    This reverts commit 7ca7171de5f607342b0272535a11d9fcf5fe5ac2.

--- server.py
+++ server.py
@@ -219,7 +219,7 @@ if __name__ == "__main__":
     ssl_kwargs['ssl_keyfile'] = cfg.ssl_keyfile
   uvicorn.run(
     "server:app",
-    host="127.0.0.1",
+    host="0.0.0.0",
     port=cfg.server_port,
     reload=False,
     log_level="info",
